c27ecc00d4520ee9c4c8982ab44f2b5cf53f3ada
Annika Chauhan <annikachauhan@Annikas-MacBook-Pro.local>
2020-02-20 18:18:02 +0000

    complete restyle

--- advanced-app.js
+++ advanced-app.js
@@ -1,11 +1,10 @@
 import stateData from "./states.js";
-console.log(stateData);
+//console.log(stateData);
 
 const gameStartUp = () => {
     let time = 0;
     let score = 0;
     const allStates = document.querySelectorAll('.states');
-    const submit = document.getElementById('submit');
     const input = document.getElementById('inputCountry');
     const matchNot = document.getElementById('matchNot');
     const scoreDisplay = document.getElementById('score');
@@ -15,34 +14,44 @@ const gameStartUp = () => {
     Object.keys(object).find(key => object[key] === value);
 
     const findCountry = () => {
-        submit.onclick = () => {
-            let stateNamed = (input.value).toLowerCase();
-            let result = getKeyByValue(stateData, stateNamed);
-            if(namedStates.includes(stateNamed)) {
-                matchNot.innerText = "You've already said this!";
-                matchNot.style.color = "black";
-            } else if (result === undefined){
-                matchNot.innerText = `${stateNamed} - not a state!`;
-                matchNot.style.color = "red";
-            } else {
-                matchNot.innerText = "It's a match!"
-                matchNot.style.color = "green";
-                score += 1;
-                namedStates.push(stateNamed);
-                let colorIn = document.getElementById(result);
-                colorIn.style.fill = "green";
-            }
-            console.log(namedStates);
-            scoreDisplay.innerText = score;
+        let stateNamed = (input.value).toLowerCase();
+        let result = getKeyByValue(stateData, stateNamed);
+        if(namedStates.includes(stateNamed)) {
+            matchNot.innerText = "You've already said this!";
+            matchNot.style.color = "black";
+        } else if (result === undefined){
+            matchNot.innerText = `"${stateNamed}" is not a state!`;
+            matchNot.style.color = "red";
+        } else {
+            matchNot.innerText = "It's a match!"
+            matchNot.style.color = "green";
+            score += 1;
+            namedStates.push(stateNamed);
+            let colorIn = document.getElementById(result);
+            colorIn.style.fill = "green";
+            stateNamed = "";
         }
+        scoreDisplay.innerText = score;   
     }
 
     const determineWin = () => {
         //if score is 52 and time is less than the allotted time then win
         // else if the time gets to the end and score is less than 52 then lose, with the option to reset
+        if(score === 5){
+            matchNot.innerText = "You win!!";
+        }
     }
 
-    findCountry();
+    document.onkeypress = () => {
+        if(event.which === 13){
+            //this should initiate the timer
+            findCountry();
+            //console.log(score);
+            determineWin();
+        }
+    }
+
+    
 
 
     const resetGame = () => {
